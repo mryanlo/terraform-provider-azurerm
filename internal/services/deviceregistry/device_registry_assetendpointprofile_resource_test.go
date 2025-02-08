09c8aafd6d5c395b585bf232b2dfd062ac160499
Ryan Lo <rylo@microsoft.com>
2025-02-07 20:16:29 -0800

    snake case aep names to fix validation errors

--- internal/services/deviceregistry/device_registry_assetendpointprofile_resource_test.go
+++ internal/services/deviceregistry/device_registry_assetendpointprofile_resource_test.go
@@ -188,7 +188,7 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_device_registry_asset_endpoint_profile" "test" {
-	name             = "myAssetEndpointProfileBasic"
+	name             = "my-assetendpointprofile-basic"
 	resource_group_name = "adr-terraform-test-113553226"
 	extended_location_name = "/subscriptions/efb15086-3322-405d-a9d0-c35715a9b722/resourceGroups/adr-terraform-test-113553226/providers/Microsoft.ExtendedLocation/customLocations/location-2h2vr"
 	extended_location_type = "CustomLocation"
@@ -208,7 +208,7 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_device_registry_asset_endpoint_profile" "test" {
-	name             = "myAssetEndpointProfileCertificate"
+	name             = "my-assetendpointprofile-certificate"
 	resource_group_name = "adr-terraform-test-113553226"
 	extended_location_name = "/subscriptions/efb15086-3322-405d-a9d0-c35715a9b722/resourceGroups/adr-terraform-test-113553226/providers/Microsoft.ExtendedLocation/customLocations/location-2h2vr"
 	extended_location_type = "CustomLocation"
@@ -232,7 +232,7 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_device_registry_asset_endpoint_profile" "test" {
-	name             = "myAssetEndpointProfileUsername"
+	name             = "my-assetendpointprofile-username"
 	resource_group_name = "adr-terraform-test-113553226"
 	extended_location_name = "/subscriptions/efb15086-3322-405d-a9d0-c35715a9b722/resourceGroups/adr-terraform-test-113553226/providers/Microsoft.ExtendedLocation/customLocations/location-2h2vr"
 	extended_location_type = "CustomLocation"
@@ -257,7 +257,7 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_device_registry_asset_endpoint_profile" "test" {
-	name             = "myAssetEndpointProfileAnonymous"
+	name             = "my-assetendpointprofile-anonymous"
 	resource_group_name = "adr-terraform-test-113553226"
 	extended_location_name = "/subscriptions/efb15086-3322-405d-a9d0-c35715a9b722/resourceGroups/adr-terraform-test-113553226/providers/Microsoft.ExtendedLocation/customLocations/location-2h2vr"
 	extended_location_type = "CustomLocation"
